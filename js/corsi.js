e60d2a23566aed7f363d53a71f8b598468b0da7b
nlm23457 <francesco.dresti@gmail.com>
2024-12-28 21:37:11 +0100

    FINITO!!

--- js/corsi.js
+++ js/corsi.js
@@ -31,6 +31,24 @@ const auth = getAuth(app);
 //****************************************************** */
 import * as dati from "./datiMartedi.js"; // Importa tutto come un oggetto
 
+const aperturaIscrizioni = new Date("2024-12-29T20:00:00Z"); // Ora in UTC
+
+fetch("https://timeapi.io/api/Time/current/zone?timeZone=Etc/UTC")
+  .then((response) => response.json())
+  .then((data) => {
+    const oraCorrente = new Date(data.dateTime); // Ora corrente in UTC
+    if (oraCorrente < aperturaIscrizioni) {
+      // Reindirizza alla pagina di attesa
+      window.location.href = "../attesa.html";
+    } else {
+      // Procedi con il caricamento normale
+      console.log("Le iscrizioni sono aperte!");
+    }
+  })
+  .catch((error) => {
+    console.error("Errore nel recuperare l'orario globale:", error);
+  });
+
 const oraGiorno = document.querySelector("#indicator").textContent.trim();
 
 const oggettoRichiesto = dati[`${oraGiorno}_corsiElenco`];
